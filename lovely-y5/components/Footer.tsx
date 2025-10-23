f2f51e4c04097fd2cc97c6f51b06305faf428fee
SibelSama <ja.torti@duocuc.cl>
2025-10-23 05:26:31 -0300

    correcciones estéticas

--- lovely-y5/components/Footer.tsx
+++ lovely-y5/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className="container py-4">
         <div className="row align-items-center">
           <div className="col-md-6 d-flex align-items-center">
-            <img src="/svgs/logo.svg" alt="Lovely Y5" width={40} height={40} />
+            <img src="lovely-y5/public/logo.svg" alt="Lovely Y5" width={40} height={40} />
             <div className="ms-3">
               <div>
                 <Link href="/"><a className="me-2">Inicio</a></Link>
@@ -20,9 +20,9 @@ export default function Footer() {
             </div>
           </div>
           <div className="col-md-6 text-md-end">
-            <a href="https://facebook.com" target="_blank" rel="noreferrer" className="me-2"><img src="/svgs/fb.svg" alt="fb" width="24" /></a>
-            <a href="https://instagram.com" target="_blank" rel="noreferrer" className="me-2"><img src="/svgs/ig.svg" alt="ig" width="24" /></a>
-            <a href="https://mercadolibre.cl" target="_blank" rel="noreferrer"><img src="/svgs/ml.svg" alt="ml" width="24" /></a>
+            <a href="https://facebook.com" target="_blank" rel="noreferrer" className="me-2"><img src="lovely-y5/public/facebook.svg" alt="fb" width="24" /></a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer" className="me-2"><img src="lovely-y5/public/instagram.svg" alt="ig" width="24" /></a>
+            <a href="https://mercadolibre.cl" target="_blank" rel="noreferrer"><img src="lovely-y5/public/mercado-libre.svg" alt="ml" width="24" /></a>
             <div className="mt-2" style={{opacity:0.8}}>Todos los derechos reservados (c) Lovely Y5 &lt;3</div>
           </div>
         </div>
