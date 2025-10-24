5636db5002d54b8b8b29ed00270b5f7fb94e4aa8
SibelSama <ja.torti@duocuc.cl>
2025-10-23 22:43:40 -0300

    intalacion de mas dependencias

--- lovely-y5/karma.conf.js
+++ lovely-y5/karma.conf.js
@@ -2,29 +2,37 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     files: [
-      'specs/**/*.spec.js'
+      './specs/**/*.spec.tsx',
+      './specs/**/*.spec.ts',
+      './specs/**/*.spec.js',
     ],
+    exclude: [],
     preprocessors: {
-      'specs/**/*.spec.js': ['webpack']
+      './specs/**/*.spec.tsx': ['webpack'],
+      './specs/**/*.spec.ts': ['webpack'],
+      './specs/**/*.spec.js': ['webpack'],
     },
     webpack: {
       mode: 'development',
+      resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
       module: {
         rules: [
           {
-            test: /\.js$/,
+            test: /\.(ts|tsx|js|jsx)$/,
             exclude: /node_modules/,
             use: {
               loader: 'babel-loader',
               options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-              }
-            }
-          }
-        ]
-      }
+                presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+              },
+            },
+          },
+        ],
+      },
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
